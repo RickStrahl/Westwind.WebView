b9fe45fea6375ef463ecf41e8ea8cd7c5359e2cf
Rick Strahl <rstrahl@west-wind.com>
2024-07-20 18:04:19 -0700

    Add WebView2Extensions.WebView2.NavigateToStringSafe()

---
+++ Westwind.WebView/Wpf/WebView2Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Web.WebView2.Wpf
+{
+    public static class WebView2Extensions
+    {
+        public static async Task NavigateToStringSafe(this WebView2 webView, string html)
+        {
+            webView.Source = new Uri("about:blank");
+
+            string encodedHtml = JsonConvert.SerializeObject(html);
+            string script = "window.document.write(" + encodedHtml + ")";
+
+            await webView.EnsureCoreWebView2Async();  // make sure WebView is ready
+            await webView.ExecuteScriptAsync(script);
+        }
+    }
+}
